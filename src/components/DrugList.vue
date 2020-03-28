bcc821013de19be8978a858e0423880bc944d3f1
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-28 12:42:14 +0100

    add actual list of drugs to supply screen

---
+++ src/components/DrugList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-card v-for="drug in drugs" :key="drug.id" class="mb-4">
+      <v-card-title>{{ drug.name }}</v-card-title>
+      <v-card-text class="text-left">{{ drug.description }}</v-card-text>
+      <v-card-actions>
+        <router-link to="/request/1">
+          <v-btn text>Add Supply</v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import http from "@/core/http";
+
+export default {
+  data() {
+    return {
+      drugs: []
+    };
+  },
+  async mounted() {
+    http.get("/medicine").then(response => {
+      this.drugs = response.data;
+    });
+  }
+};
+</script>
--- src/views/Supply.vue
+++ src/views/Supply.vue
@@ -8,12 +8,27 @@
           <v-btn block raised color="primary" class="mt-10">Update</v-btn>
         </router-link>
 
-        <router-link to="/supply/1/add">
+        <v-divider class="my-10"></v-divider>
+
+        <h2>Can you supply any of these drugs?</h2>
+
+        <drug-list></drug-list>
+        <!-- <router-link to="/supply/1/add">
           <v-btn block raised color="primary" class="mt-10">Add</v-btn>
-        </router-link>
+        </router-link> -->
 
         <router-link to="/">back</router-link>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import DrugList from '@/components/DrugList';
+
+export default {
+  components: {
+    DrugList
+  }
+}
+</script>
