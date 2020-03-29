9096e229b66e8fd7ac84952c6b63f5b5bfe728bf
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-29 13:12:04 +0200

    update app header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <router-link to="/">
-        <v-img
-          alt="OpenMedStock Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/logo.png"
-          transition="scale-transition"
-          width="64"
-        />
-      </router-link>
-
-      <v-btn text class="ml-5" @click="goBack" v-if="notOnIntro">
+      <v-spacer></v-spacer>
+      <v-btn text @click="goBack">
         <v-icon left>fas fa-chevron-left</v-icon> back
       </v-btn>
 
       <v-spacer></v-spacer>
 
+      <router-link to="/">
+        <v-img
+          alt="OpenMedStock Logo"
+          class="shrink"
+          contain
+          src="@/assets/logo-wide.png"
+          transition="scale-transition"
+          height="40"
+        />
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-content>
+      <v-row no-gutters class="mt-5">
+        <v-col lg="6" md="8" offset-lg="3" offset-md="2">
+          <router-view></router-view>
+        </v-col>
+      </v-row>
+    </v-content>
+
+    <v-bottom-navigation>
       <v-btn
         href="https://devpost.com/software/openmedstock"
         target="_blank"
@@ -32,18 +46,10 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Github</span>
+      <span class="mr-2">Github</span>
         <v-icon>fab fa-github</v-icon>
       </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <v-row no-gutters class="mt-5">
-        <v-col lg="6" md="8" offset-lg="3" offset-md="2">
-          <router-view></router-view>
-        </v-col>
-      </v-row>
-    </v-content>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
