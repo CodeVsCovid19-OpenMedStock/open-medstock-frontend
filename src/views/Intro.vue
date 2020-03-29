3037dd7a60cbd6189d05c4b5dcdacccdf7e9e060
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-29 19:20:11 +0200

    simplify markup on intro page

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,25 +1,21 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="headline font-weight-bold mb-3">Welcome to OpenMedStock</h1>
+  <v-container class="text-center">
+    <h1 class="headline font-weight-bold mb-3">Welcome to OpenMedStock</h1>
 
-        <h2 class="title font-weight-light font-italic mb-4">
-          Exchange life-saving meds
-        </h2>
+    <h2 class="title font-weight-light font-italic mb-4">
+      Exchange life-saving meds
+    </h2>
 
-        <p>Please select wheather you can provide or need meds</p>
+    <p>Please select wheather you can provide or need meds</p>
 
-        <router-link to="/request">
-          <v-btn block raised color="primary" class="mt-10">I need meds</v-btn>
-        </router-link>
+    <router-link to="/request">
+      <v-btn block raised color="primary" class="mt-10">I need meds</v-btn>
+    </router-link>
 
-        <router-link to="/supply">
-          <v-btn block raised color="primary" class="mt-10">
-            I can provide meds
-          </v-btn>
-        </router-link>
-      </v-col>
-    </v-row>
+    <router-link to="/supply">
+      <v-btn block raised color="primary" class="mt-10">
+        I can provide meds
+      </v-btn>
+    </router-link>
   </v-container>
 </template>
