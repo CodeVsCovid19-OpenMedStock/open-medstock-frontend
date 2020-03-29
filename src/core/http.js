57f7c6d624d30b1b13ad6a8e99b352fca7fb8fd5
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-29 19:15:55 +0200

    fallback to prod url if env variable isn't available

--- src/core/http.js
+++ src/core/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL
+  baseURL: process.env.VUE_APP_API_URL || "https://api.openmedstock.org"
 });
 
 http.interceptors.request.use(config => {
