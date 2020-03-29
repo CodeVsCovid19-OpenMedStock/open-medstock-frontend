2da572462e41fc56b43d58abe9c8de0f5139d9f5
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-29 13:12:17 +0200

    configure local and production api urls

--- src/core/http.js
+++ src/core/http.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: process.env.VUE_APP_API_URL
+});
+
+http.interceptors.request.use(config => {
+  config.headers["Authorization"] = "Bearer 1";
+  return config;
 });
 
 export default http;
