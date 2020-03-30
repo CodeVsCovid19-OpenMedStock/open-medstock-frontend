4577d29275ae82aeb3c423c34fe3be8bdeeeb038
Christof Leuenberger <christof.leuenberger@mobi.ch>
2020-03-30 13:02:55 +0200

    add notice to login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,13 +4,18 @@
 
     <p>Please log in first in order to register your supply</p>
 
+    <v-alert
+      text
+      type="info"
+    >
+      Login is not enabled for the prototype. Please just click "Login" to proceed.
+    </v-alert>
+
     <v-form class="mt-4">
       <v-text-field label="E-Mail" required></v-text-field>
       <v-text-field label="Password" type="password" required></v-text-field>
 
-      <v-btn block class="mt-4" color="primary" :disabled="busy" @click="login">
-        Login
-      </v-btn>
+      <v-btn block class="mt-4" color="primary" :disabled="busy" @click="login">Login</v-btn>
     </v-form>
   </v-container>
 </template>
